a31e31c6d0022c79506c0b7e7b7738a29b53cc27
Feng DING <dingbobby@hotmail.com>
2020-02-03 11:46:17 +0800

    <fix>(pack): add plugins for app

--- pack.sh
+++ pack.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
-# 可执行程序名
+# params
 appname="point_cloud_player"
 dst="./app"
+plugins="plugins"
+readme="readme.md"
 rm -rf $dst.tar.gz $dst
 mkdir $dst
-# 利用 ldd 提取依赖库的具体路径
+# use ldd extract libs
 liblist=$(ldd $appname | awk '{ if (match($3,"/")){ printf("%s "), $3 } }')
-# 目标文件夹的检测
+
 if [ ! -d $dst ];then
 		mkdir $dst
 fi
-# 拷贝库文件和可执行程序到目标文件夹
+# cp files
 cp $liblist $dst
 cp $appname $dst
+cp $readme $dst
+cp -r $plugins $dst
+# pack
 tar -zcvf $dst.tar.gz $dst
+#remove
 rm -rf $dst
